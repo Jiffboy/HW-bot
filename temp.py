5de92021dcde391c30c52a80314d92f54310ad51
Jeff <jeffreyrosscs@gmail.com>
2019-05-14 11:28:25 -0600

    Now pings users on startup, with event id from command line, and closes itself after pinging

--- temp.py
+++ temp.py
@@ -1,10 +1,13 @@
 import requests 
 import discord
 import json
+import sys
+
+event = sys.argv[1]
 
 #Connect to website and pull json for specified event
 url = "https://imehi.me/participants/"
-data = {'event':9} 
+data = {'event':event} 
 response = requests.post(url = url, data = data)
 json = json.loads(response.text)
 
@@ -21,26 +24,17 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
+    print('I have connected')
     guild = await client.fetch_guild(532437793805303808)
-    channels = guild.text_channels
+    channels = client.get_all_channels()
     for channel in channels:
-        if channel.id == 534093319089946629:
-            channel.send('hello')
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('!yeet'):
-        await message.channel.send('Yeet')
-    
-    if message.content.startswith('!test'):
-        guild = await client.fetch_guild(532437793805303808)
-        for id in pingList:
-            user = await guild.fetch_member(id)
-            await message.channel.send(user.mention)
-
+        if channel.id == 577895915319459862:
+            message = ""
+            for id in pingList:
+                user = await guild.fetch_member(id)
+                message += user.mention + " "
+            await channel.send(message)
+            sys.exit()
         
 client.run(token.strip())
 
